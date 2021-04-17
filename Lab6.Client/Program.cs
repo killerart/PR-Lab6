c030bd7dc67b238d1495c6fd3ea517f4df561a6a
killerart <442234@mail.ru>
2021-04-17 18:40:56 +0300

    Add project files.

---
+++ Lab6.Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6.Client
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            string host = "127.0.0.1";
+            var hostIP = IPAddress.Parse(host);
+            const int serverPort = 10000;
+            var serverEP = new IPEndPoint(hostIP, serverPort);
+
+            var tasks = Enumerable.Range(10001, 100).Select(port => Task.Run(() =>
+            {
+                try
+                {
+
+                    var EP = new IPEndPoint(hostIP, port);
+
+                    using var client = new UdpClient(EP);
+
+                    var message = "Hello there";
+                    var bytes = Encoding.Default.GetBytes(message);
+                    client.Send(bytes, bytes.Length, serverEP);
+                    Console.WriteLine($"Message '{message}' sent from {EP} to {serverEP}");
+
+                    IPEndPoint remoteEP = null;
+                    bytes = client.Receive(ref remoteEP);
+                    message = Encoding.Default.GetString(bytes);
+                    Console.WriteLine($"Message '{message}' received from {remoteEP} to {EP}\n");
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }));
+            await Task.WhenAll(tasks);
+        }
+    }
+}
---
+++ Lab6.Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.Server
+{
+    class Program
+    {
+        static readonly string host = "127.0.0.1";
+        const int port = 10000;
+        static readonly IPEndPoint EP = new(IPAddress.Parse(host), port);
+        static readonly UdpClient listener = new(EP);
+        static void Main()
+        {
+            listener.BeginReceive(HandleConnection, listener);
+            Console.WriteLine($"Server listening for UPD packets on port {port}\n");
+            Console.ReadKey(true);
+        }
+
+        static void HandleConnection(IAsyncResult result)
+        {
+            listener.BeginReceive(HandleConnection, listener);
+            IPEndPoint remoteEP = null;
+            var bytes = listener.EndReceive(result, ref remoteEP);
+            var message = Encoding.Default.GetString(bytes);
+            Console.WriteLine($"Message '{message}' received from {remoteEP}");
+
+            message = string.Join(string.Empty, message.Reverse());
+            bytes = Encoding.Default.GetBytes(message);
+            listener.Send(bytes, bytes.Length, remoteEP);
+            Console.WriteLine($"Message '{message}' sent to {remoteEP}\n");
+        }
+    }
+}
